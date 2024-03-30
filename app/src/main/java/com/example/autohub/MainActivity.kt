a15aa2abb9518386061ae8a2e53198f3f8325da8
Daniil <danilmega2003@gmail.com>
2024-03-30 15:44:20 +0300

    Добавлены nav_graph

--- app/src/main/java/com/example/autohub/MainActivity.kt
+++ app/src/main/java/com/example/autohub/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.autohub
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+
 }
