37d5a49a028b22a677d32da650d973b0fb332182
Daniil <danilmega2003@gmail.com>
2024-04-16 20:49:07 +0300

    Добавлен класс Car, адаптер для списка машин и разметка элемента списка

---
+++ app/src/main/java/com/example/autohub/Car.kt
@@ -0,0 +1,14 @@
+package com.example.autohub
+
+data class Car(
+    val make: String,
+    val model: String,
+    val year: Int,
+    val price: String,
+    val mileage: String,
+    val bodyType: String,
+    val engineType: String,
+    val color: String,
+    val transmission: String,
+    val driveline: String
+)
---
+++ app/src/main/java/com/example/autohub/CarAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.autohub
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CarAdapter : RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
+
+    var carList = emptyList<Car>()
+
+    class CarViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.car_list_item, parent, false)
+        return CarViewHolder(view)
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+        val carInfo = carList[position]
+        holder.itemView.findViewById<TextView>(R.id.car_title).text =
+            carInfo.make + " " + carInfo.model + " " + carInfo.year
+        holder.itemView.findViewById<TextView>(R.id.car_characteristics).text =
+            carInfo.mileage + " " + carInfo.bodyType + " " + carInfo.transmission + " " +
+                    carInfo.engineType + " " + carInfo.driveline
+        holder.itemView.findViewById<TextView>(R.id.car_price).text = carInfo.price
+    }
+
+    override fun getItemCount(): Int {
+        return carList.size
+    }
+}
